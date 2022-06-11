a56d1010109f70092c159f003bbe0b93cb4797f1
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-11 20:26:13 +0200

    Personal route created

--- src/components/hero/HeroCard.js
+++ src/components/hero/HeroCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const HeroCard = ({
     id,
@@ -8,13 +8,32 @@ export const HeroCard = ({
     first_appearance,
     characters
 }) => {
+
+    const imgPath = `/assets/${id}.jpg`
+
     return (
         <div className='col'>
-        <div className='card'>
-            <div className='row no-gutters'>
-                <img src={`/assets/${id}.jpg`} className='card-img-top' alt={superhero} />
+            <div className='card'>
+                <div className='row no-gutters'>
+                    <div className='col-4'>
+                        <img src={imgPath} className='card-img' alt={superhero} />
+                    </div>
+                    <div className='col-8'>
+                        <div className='card-body'>
+                            <h5 className='card-title'>{superhero}</h5>
+                            <p className='card-text'>{alter_ego}</p>
+                            {
+                                (alter_ego !== characters)
+                                && <p className='text-muted'>{characters}</p>
+                            }
+                            <p className='card-text'>{first_appearance}</p>
+                            <Link to={`/hero/${id}`}>
+                                Más...
+                            </Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -36,13 +36,7 @@ export const Navbar = () => {
                     >
                         DC
                     </NavLink>
-
-                    <NavLink
-                        className={({ isActive }) => 'nav-item nav-link ' + (isActive ? 'active' : '')}
-                        to='/hero'
-                    >
-                        Hero
-                    </NavLink>
+                    
                 </div>
             </div>
 
