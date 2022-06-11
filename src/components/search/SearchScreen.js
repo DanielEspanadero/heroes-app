82268d2304942dc229a90a0da655c2d64e271515
Daniel Españadero <developer@daniel-espanadero.com>
2022-06-12 00:12:08 +0200

    Heroes List

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,49 +1,66 @@
-import { useForm } from "../../hooks/useForm"
+import { useForm } from '../../hooks/useForm';
+import { getHeroesByName } from '../../selector/getHeroesByName';
+import {heroCard} from '../hero/HeroCard';
 
 export const SearchScreen = () => {
 
     const [formValues, handleInputChange] = useForm({
         searchText: ''
-    })
+    });
 
-    const {searchText} = formValues
+    const { searchText } = formValues;
+    const heroesFiltered = getHeroesByName('Aaa');
 
     const handleSearch = (e) => {
         e.preventDefault();
         console.log(searchText);
-    }
+    };
 
     return (
         <>
             <h1>Búsquedas</h1>
             <hr />
 
-            <div className="row">
-                <div className="col-5">
+            <div className='row'>
+                <div className='col-5'>
                     <h4>Buscar</h4>
                     <hr />
 
                     <form onSubmit={handleSearch}>
                         <input
                             type='text'
-                            placeholder="Buscar un héroe"
-                            className="form-control"
-                            name="searchText"
-                            autoComplete="off"
+                            placeholder='Buscar un héroe'
+                            className='form-control'
+                            name='searchText'
+                            autoComplete='off'
                             value={searchText}
                             onChange={handleInputChange}
                         >
                         </input>
                         <button
-                            className="btn btn-outline-prymary mt-1"
-                            type="submit"
+                            className='btn btn-outline-prymary mt-1'
+                            type='submit'
                         >
                             Buscar...
                         </button>
 
                     </form>
                 </div>
+                <div className='col-7'>
+                    <h4>Resultados</h4>
+                    <hr />
+
+                    {
+                        heroesFiltered.map(hero => {
+                            <heroCard
+                            key={hero.id}
+                            {...hero}
+                            />
+                        })
+                    }
+
+                </div>
             </div>
         </>
-    )
-}
+    );
+};
